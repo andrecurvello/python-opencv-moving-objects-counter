1a2ffcbea55339727cdc632daa430376c768e799
Hallgeir Wilhelmsen <wilhelmsen@gmail.com>
2013-02-11 14:32:24 +0100

    Adding argsparse and using the video capturer...

--- backgextract.py
+++ backgextract.py
@@ -1,66 +1,45 @@
 #!/usr/bin/python
 import cv2
 import numpy as np
-c = cv2.VideoCapture(0)
-_,f = c.read()
+import video_capturer
+import logging
+LOG = logging.getLogger()
+
 
 def color2gray(img):
     return cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-def resize(img,factor):
-    imgsize=img.shape
-    imgwidth=int(imgsize[1]*factor)
-    imgheight=int(imgsize[0]*factor)
-    return cv2.resize(img,(imgwidth,imgheight))
+def main():
+    with video_capturer.video_capturer() as v:
+        while( True ):
+            f = v.get_frame()
+            background = v.get_background()
+            
+            dif = cv2.absdiff(background,f)
+            res2 = cv2.convertScaleAbs(dif)
+            res2 = color2gray(res2)
+            
+            _,thresh = cv2.threshold(res2, 20, 255, cv2.THRESH_BINARY)
+            
+#            cv2.imshow('img',f);
+#            cv2.imshow('background',background)
+            cv2.imshow('res',thresh)
 
-def preprocess(img):
-    smooth = cv2.GaussianBlur(img,(11,11),0)
-    return smooth
+            k = cv2.waitKey(20)
+            
+            if k == 27:
+                break
 
-factor = 0.5
+if __name__ == "__main__":
+    import argparse
 
-f = resize(f, factor)
-avg = np.float32(f)
-dif = np.float32(f)
-
-numchanged = 0
-numdetect = 0
-while(1):
-    _,f = c.read()
-
-    f = resize(f,factor)
-    f = preprocess(f)
-
-    numchangedthreshold = 4000
-    if numchanged > numchangedthreshold:
-        alpha = 0.01
-        if event:
-            numdetect += 1
-            print("numdetect=%i" % numdetect)
-            event = False
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-d", "--debug", action="store_true")
+    args = parser.parse_args()
+    
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
     else:
-        alpha = 0.5
-        event = True
-
-    cv2.accumulateWeighted(f,avg,alpha)
-    res1 = cv2.convertScaleAbs(avg)
- 
-    dif = cv2.absdiff(res1,f)
-    res2 = cv2.convertScaleAbs(dif);
-    res2 = color2gray(res2)
-
-    _,thresh = cv2.threshold(res2, 20, 255, cv2.THRESH_BINARY)
-
-    numchanged = cv2.countNonZero(thresh) 
-
-    cv2.imshow('img',f);
-    cv2.imshow('avg',res1)
-    cv2.imshow('res',thresh)
-
-    k = cv2.waitKey(20)
- 
-    if k == 27:
-        break
- 
-cv2.destroyAllWindows()
-c.release()
+        logging.basicConfig(level=logging.INFO)        
+    main()
--- video_capturer.py
+++ video_capturer.py
@@ -1,35 +1,66 @@
 import cv2
 import logging
+import numpy as np
 LOG = logging.getLogger(__name__)
 
 
 class video_capturer():
-    def __enter__(self):
+    def __init__( self ):
         self.c = cv2.VideoCapture(0)
         self.resize_factor = 0.5
         self.blur_kernel_size = 11 # (int(np.max(frame.shape)*0.04)/2)*2+1 # Allways an odd number
+        self.alpha = 0.001
+        self.background = np.float32(self._get_frame())
+        self.avg = self.background
+
+    def __enter__( self ):
+        """ What to do when entering the with statement."""
+        # Makes the background initialization a bit quicker. Uncomment the next line.
+        # self.init_background() 
         return self
 
     def __exit__(self, type, value, traceback):
+        """ What to do when exiting the with statement. """
         cv2.destroyAllWindows()
         self.c.release()
 
+    def init_background(self):
+        # Save original value
+        orig_alpha = self.alpha
+        self.alpha = 1.0  # Just some high alpha value.
+        while self.alpha > orig_alpha:
+            self.get_frame() # Just to get one with it...
+            self.alpha -= self.alpha/10.0 # Reduce by 10%.
+            LOG.debug("New alpha: '%f'"%(self.alpha))
+        self.alpha = orig_alpha
+
+    def resize(self, img, factor):
+        imgsize=img.shape
+        imgwidth=int(imgsize[1]*factor)
+        imgheight=int(imgsize[0]*factor)
+        LOG.debug( "Image resized to (%f,%f)"%(imgwidth, imgheight) )
+        return cv2.resize(img,(imgwidth,imgheight))
+
+    def preprocess(self, img):
+        kernel_size = (self.blur_kernel_size,self.blur_kernel_size)
+        sigma = 0
+        smooth = cv2.GaussianBlur( img, kernel_size, sigma )
+        return smooth
+
     def get_frame(self):
+        frame = self._get_frame()
+        self.accumulate_background( frame )
+        return frame
+
+    def _get_frame(self):
         _,frame = self.c.read()
         frame = self.resize(frame, self.resize_factor)
         frame = self.preprocess(frame)
         return frame
 
-    def resize(self, img, factor):
-        imgsize=img.shape
-        LOG.debug( imgsize )
-        imgwidth=int(imgsize[1]*factor)
-        imgheight=int(imgsize[0]*factor)
-        return cv2.resize(img,(imgwidth,imgheight))
+    def accumulate_background( self, frame ):
+        cv2.accumulateWeighted( frame, self.avg, self.alpha )
+        self.background = cv2.convertScaleAbs( self.avg )
 
-    def preprocess(self, img):
-        LOG.debug( self.blur_kernel_size )
-        kernel_size = (self.blur_kernel_size,self.blur_kernel_size)
-        sigma = 0
-        smooth = cv2.GaussianBlur( img, kernel_size, sigma )
-        return smooth
+    def get_background(self):
+        return self.background
